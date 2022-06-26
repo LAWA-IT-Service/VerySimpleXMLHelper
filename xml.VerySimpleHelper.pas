520b721977f4c82e1d2cd89fc849ab821b9a31b0
LAWA-IT-Service <75973662+LAWA-IT-Service@users.noreply.github.com>
2022-06-26 16:25:53 +0200

    Update Header

--- xml.VerySimpleHelper.pas
+++ xml.VerySimpleHelper.pas
@@ -1,19 +1,13 @@
 { VerySimpleXMLHelper v0.0.1 
   (c) Copyrights 2021  <it@lawa-frische.de>									
-  This unit is free and can be used for any needs. The introduction of		
-  any changes and the use of those changed library is permitted without		
-  limitations. Only requirement:                            					
-  This text must be present without changes in all modifications of library. 	
-  * The contents of this file are used with permission, subject to
-  * the Mozilla Public License Version 1.1 (the "License"); you may   *
-  * not use this file except in compliance with the License. You may  *
-  * obtain a copy of the License at                                   *
-  * http:  www.mozilla.org/MPL/MPL-1.1.html                           *
-  *                                                                   *
-  * Software distributed under the License is distributed on an       *
-  * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or    *
-  * implied. See the License for the specific language governing      *
-  * rights and limitations under the License.                         *
+  * This Source Code Form is subject to the terms of the Mozilla Public *
+  * License, v. 2.0. If a copy of the MPL was not distributed with this *
+  * file, You can obtain one at https://mozilla.org/MPL/2.0/.           *
+  *                                                                     *
+  *   Software distributed under the License is distributed on an       *
+  *   "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or    *
+  *   implied. See the License for the specific language governing      *
+  *   rights and limitations under the License.                         *
 }
 
 unit xml.VerySimpleHelper;
